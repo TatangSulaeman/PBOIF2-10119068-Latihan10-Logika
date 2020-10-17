9504fce1de1b92371b6834bfeda1cdadef5f12c3
amtat <amtat@DESKTOP-3DL3458>
2020-10-17 17:36:32 +0700

    PBOIF2-10119068-Latihan10-Logika

---
+++ src/pboif2/pkg10119068/latihan10/logika/PBOIF210119068Latihan10Logika.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119068.latihan10.logika;
+
+/**
+ *
+ * @author amtat
+ * NAMA     : TATANG SULAEMAN
+ * KELAS    : PBOIF2
+ * NIM      : 10119068
+ * Deskripsi program    : Logika
+ */
+public class PBOIF210119068Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >=17);
+        System.out.println("Statu Dewasa : " + dewasa );
+    }
+    
+}
